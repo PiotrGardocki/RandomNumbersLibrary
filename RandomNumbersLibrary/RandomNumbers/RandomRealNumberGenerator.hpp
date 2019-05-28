4657ea288109381538a041ad765d02104414cd3c
Piotr Gardocki <32600711+PiotrGardocki@users.noreply.github.com>
2019-05-28 22:52:56 +0200

    Fixed formatting

--- RandomNumbersLibrary/RandomNumbers/RandomRealNumberGenerator.hpp
+++ RandomNumbersLibrary/RandomNumbers/RandomRealNumberGenerator.hpp
@@ -10,21 +10,21 @@ class RandomRealNumberGenerator : private RandomGeneratorBase
 {
 	static_assert(std::is_floating_point<T>::value, "RandomRealNumberGenerator has to be floating point type");
 
-    public:
-		RandomRealNumberGenerator(const T & min, const T & max);
+public:
+	RandomRealNumberGenerator(const T & min, const T & max);
 
-		void setNewRange(const T & min, const T & max);
-		void setNewRange(const std::pair<T, T> & range);
+	void setNewRange(const T & min, const T & max);
+	void setNewRange(const std::pair<T, T> & range);
 
-		T getMin() const;
-		T getMax() const;
-		std::pair<T, T> getRange() const;
+	T getMin() const;
+	T getMax() const;
+	std::pair<T, T> getRange() const;
 
-		T generateRandom();
-		T operator()();
+	T generateRandom();
+	T operator()();
 
-    private:
-		std::uniform_real_distribution<T> mRandom;
+private:
+	std::uniform_real_distribution<T> mRandom;
 };
 
 #include "RandomRealNumberGenerator.inl"
